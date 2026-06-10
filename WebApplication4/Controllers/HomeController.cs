224f1a49c4260a1073a91fcdcc90d18ca0ac5eb1
DanielMitevGitHub <mitev.daniel84@gmail.com>
2026-06-10 11:29:51 +0200

    Finale Version mit Azure Deployment

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using MVC_Eurobanknoten.Models;
 using WebApplication4.Models;
 
-namespace WebApplication4.Controllers
+
+namespace Banknotenprüfung.Controllers
 {
     public class HomeController : Controller
     {
@@ -22,12 +23,67 @@ namespace WebApplication4.Controllers
         [HttpPost]
         public IActionResult Einzelprüfung(string serialNumber)
         {
+            if (string.IsNullOrWhiteSpace(serialNumber))
+            {
+                ViewBag.Result = "Bitte geben Sie eine Seriennummer ein.";
+                return View("Index");
+            }
+
             Seriennummerprüfer checker = new Seriennummerprüfer();
             string result = checker.CheckSerial(serialNumber);
             ViewBag.Result = result;
             return View("Index");
         }
 
+        [HttpPost]
+        public IActionResult Listenprüfung(string serialNumbers)
+        {
+
+            if (string.IsNullOrWhiteSpace(serialNumbers))
+            {
+                ViewBag.Result = "Bitte geben Sie eine Seriennummer ein.";
+                return View("Index");
+            }
+            Seriennummerprüfer checker = new Seriennummerprüfer();
+            string[] serialNumberArray = serialNumbers.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> results = new List<string>();
+            foreach (string serialNumber in serialNumberArray)
+            {
+                string result = checker.CheckSerial(serialNumber);
+                results.Add(result);
+            }
+            ViewBag.Results = results;
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Dateiprüfung(IFormFile datei)
+        {
+            if(datei == null || datei.Length == 0)
+            {
+                ViewBag.Result = "Bitte laden Sie eine Datei hoch.";
+                return View("Index");
+            }
+
+            Seriennummerprüfer checker = new Seriennummerprüfer();
+
+            List<string> results = new List<string>();
+
+            if (datei != null && datei.Length > 0)
+            {
+                using (var reader = new StreamReader(datei.OpenReadStream()))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string serialNumber = reader.ReadLine();
+                        string result = checker.CheckSerial(serialNumber);
+                        results.Add(result);
+                    }
+                }
+            }
+            ViewBag.Results = results;
+            return View("Index");
+        }
         public IActionResult Privacy()
         {
             return View();
